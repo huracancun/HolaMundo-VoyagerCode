59086ab237209c1d76950b86fc6030f850e3c45f
huracancun <a.dre@outlook.com>
2017-02-13 20:22:09 -0600

    HolaMundo

---
+++ HolaMundo2.0/src/holamundo2/pkg0/HolaMundo20.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo2.pkg0;
+
+/**
+ *
+ * @author Alumno
+ */
+public class HolaMundo20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        System.out.println("Hola Mundo, equipo 3 en GIT HUB...Segun");
+    }}
+    
